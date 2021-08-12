1499cfb7c110a14f4b046e9a57ddc6bd7f24627e
Salah Adawi <salah.adawi@outlook.com>
2021-08-12 19:36:39 +0300

    exercise 1.8

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,23 @@
 import React, { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  let all = good + neutral + bad
+  let average = (good - bad) / all
+  let positive = good / all
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+      <Display text="all" value={all} />
+      <Display text="average" value={average} />
+      <Display text="positive" value={positive + " %"} />
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -9,23 +27,13 @@ const App = () => {
   const increaseNeutral = () => setNeutral(neutral + 1)
   const increaseBad = () => setBad(bad + 1)
 
-  let all = good + neutral + bad
-  let average = (good - bad) / all
-  let positive = good / all
-
   return (
     <div>
       <h1>give feedback</h1>
       <Button onClick={increaseGood}text="good"/>
       <Button onClick={increaseNeutral}text="neutral"/>
       <Button onClick={increaseBad}text="bad"/>
-      <h1>statistics</h1>
-      <Display text="good" value={good}/>
-      <Display text="neutral" value={neutral}/>
-      <Display text="bad" value={bad}/>
-      <Display text="all" value={all}/>
-      <Display text="average" value={average}/>
-      <Display text="positive" value={positive + " %"}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
