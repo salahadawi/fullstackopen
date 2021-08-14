92b9794049a0e61eb0e35e1f00c1d130ff7c38df
Salah Adawi <salah.adawi@outlook.com>
2021-08-14 12:49:23 +0300

    move to separate folder, fix warning

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,12 +16,14 @@ const Statistics = ({good, neutral, bad}) => {
     <div>
       <h1>statistics</h1>
       <table>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive + " %"} />
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + " %"} />
+        </tbody>
       </table>
     </div>
   )
