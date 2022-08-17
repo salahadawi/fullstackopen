3e997a839e6820c006fd7bd42129a84eaeb9ed5e
Salah Adawi <salah.adawi@outlook.com>
2022-08-17 21:50:27 +0300

    part 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,24 +9,28 @@ const DisplayCountry = ({country}) => (
     <h2>languages:</h2>
     <ul>
       {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
-      {/* {[country.languages].map(language => <li key={language.name.common}>{language.name.common}</li>)} */}
     </ul>
     <img src={country.flags.png} alt={country.name.common} width="200"/>
   </>
 )
 
-const Display = ({input, countries}) => {
+const Display = ({input, countries, countriesShow, setCountriesShow}) => {
+  const onShow = (country) => {
+    setCountriesShow(countriesShow.concat(country))
+  }
+
   if (input.length === 0)
     return null
   let matchedCountries = countries.filter(country => country.name.common.toLowerCase().includes(input.toLowerCase()))
   if (matchedCountries.length > 10)
     return <p>too many matches, specify another filter</p>
   if (matchedCountries.length === 1)
-  {
-    console.log(matchedCountries[0])
     return <DisplayCountry country={matchedCountries[0]}/>
+  return <ul>{matchedCountries.map(country => {
+    return countriesShow.includes(country.name.common) ? <DisplayCountry country={country} key={country.name.common}/> :
+  <li key={country.name.common}>{country.name.common} <button onClick={() => onShow(country.name.common)}>show</button></li>
   }
-  return <ul>{matchedCountries.map(country => <li key={country.name.common}>{country.name.common}</li>)}</ul>
+  )}</ul>
 }
 
 const App = () => {
@@ -34,12 +38,12 @@ const App = () => {
 
   const [countries, setCountries] = useState([])
 
+  const [countriesShow, setCountriesShow] = useState([])
+
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
-        console.log('promise fulfilled')
         setCountries(response.data)
       })
   }, [])
@@ -47,12 +51,13 @@ const App = () => {
   const handleInputFindChange = (event) => {
     event.preventDefault()
     setInputFind(event.target.value)
+    setCountriesShow([])
   }
 
   return (
     <>
       <p>find countries <input onChange={handleInputFindChange} value={inputFind}/></p>
-      <Display input={inputFind} countries={countries}/>
+      <Display input={inputFind} countries={countries} countriesShow={countriesShow} setCountriesShow={setCountriesShow}/>
     </>
   )
 }
