838313f69c33e18e3ef299da9d1fe65af6bf65e0
Salah Adawi <salah.adawi@outlook.com>
2022-08-17 12:09:47 +0300

    part 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,62 +1,28 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
+import { useState } from 'react'
 
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </>
-  )
-}
+const Button = (props) =>
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
 
-const Content = (props) => {
-  return (
-    <>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </>
-  )
-}
+const Display = ({text, amount}) => <div>{text} {amount}</div>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <h1>give feedback</h1>
+      <Button text='good' handleClick={() => setGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      <Display text='good' amount={good} />
+      <Display text='neutral' amount={neutral} />
+      <Display text='bad' amount={bad} />
     </>
   )
 }
