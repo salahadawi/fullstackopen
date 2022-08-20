8c877b12fe7951d56dcc6c8f439c1c3eb5e622cd
Salah Adawi <salah.adawi@outlook.com>
2022-08-20 13:51:36 +0300

    part 2.19

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -24,11 +24,24 @@ const Persons = ({ persons, filter, deletePerson}) => (
   </ul>
 )
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='success'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
   const handleNameChange = (event) => {
     event.preventDefault()
@@ -76,6 +89,12 @@ const App = () => {
           setNewNumber('')
         })
     }
+    setMessage(
+      `Added ${newName}`
+    )
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
   }
 
   const deletePerson = (id) => {
@@ -104,6 +123,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter filter={filter} onChange={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
