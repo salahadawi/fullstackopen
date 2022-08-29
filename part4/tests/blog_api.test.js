181dbe0655253a04149ae1a1d145067dcc022f6d
Salah Adawi <salah.adawi@outlook.com>
2022-08-29 18:48:23 +0300

    part 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
