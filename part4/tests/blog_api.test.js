98b1b7a9215152d6f4f5f13d76faae921e90d202
Salah Adawi <salah.adawi@outlook.com>
2022-08-30 14:03:24 +0300

    remove faulty double require

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const blog = require('../models/blog')
 const Blog = require('../models/blog')
 const helper = require('./test_helper')
 
