c895f0b92edd03c0f88399dcabcabb0dd7fe2ac7
Salah Adawi <salah.adawi@outlook.com>
2022-08-31 19:15:25 +0300

    part 5.2

--- part5/src/App.js
+++ part5/src/App.js
@@ -15,6 +15,15 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -35,32 +44,43 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  const logOutButton = () => (
+    <button onClick={handleLogout}>logout</button>
+  )
+
   const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
-      </div>
-      <div>
-        password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
-      </div>
-      <button type="submit">login</button>
-    </form>
+    <>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
   )
 
   const blogDisplay = () => (
     <>
       <h2>blogs</h2>
+      <p>{user.name} logged in {logOutButton()}</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </>
   )
 
-
   return (
     <div>
       {!user && loginForm()}
-      {user && <h1>{user.name}</h1>}
       {user && blogDisplay()}
     </div>
   )
