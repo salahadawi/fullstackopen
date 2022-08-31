6e0bf20eaf825b53c8ff056dc5f564a3f76be212
Salah Adawi <salah.adawi@outlook.com>
2022-08-31 10:18:19 +0300

    clarify error messages

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -8,9 +8,9 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({ error: 'invalid token' })
+    return response.status(401).json({ error: 'invalid authorization token' })
   } else if (error.name === 'TokenExpiredError') {
-    return response.status(401).json({ error: 'token expired' })
+    return response.status(401).json({ error: 'authorization token expired' })
   }
   next(error)
 }
