fd7001268a69d3ab47db7e8c5c918bce4c93224a
Salah Adawi <salah.adawi@outlook.com>
2022-09-01 15:05:43 +0300

    part 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button>
+        <div>{blog.url}</div>
+        <div>{blog.likes} likes <button onClick={() => blog.likes++}>like</button></div>
+        { blog.user ? <div>added by {blog.user.name}</div> : null }
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+      </div>
+    )
+
+    // return (
+    //   <div style={blogStyle}>
+    //     {blog.title} {blog.author}
+    //   </div>
+    // )
+  }
+}
 
 export default Blog
