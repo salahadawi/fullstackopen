8e25b06c1553298a58dd8e90468e078661663373
Salah Adawi <salah.adawi@outlook.com>
2022-09-02 14:58:33 +0300

    part 5.18

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,37 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users/', {
+      username: 'testuser',
+      name: 'Test User',
+      password: 'salainen'
+    })
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('log in')
   })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+    })
+  })
+
 })
--- part5/src/components/LoginForm.js
+++ part5/src/components/LoginForm.js
@@ -2,12 +2,12 @@ const LoginForm = ({ handleLogin, username, password, setUsername, setPassword }
   <>
     <form onSubmit={handleLogin}>
       <div>
-        username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+        username <input id="username" type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
       </div>
       <div>
-        password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+        password <input id="password" type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">login</button>
     </form>
   </>
 )
