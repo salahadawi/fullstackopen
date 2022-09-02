62362ed5ed71467c71c9a61422aeb09799ac215b
Salah Adawi <salah.adawi@outlook.com>
2022-09-02 15:13:45 +0300

    part 5.19 and 5.20

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,5 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.request('POST', 'http://localhost:3003/api/users/', {
       username: 'testuser',
@@ -9,12 +9,12 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('log in')
   })
 
-  describe('Login', function() {
-    it('succeeds with correct credentials', function() {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
       cy.get('#username').type('testuser')
       cy.get('#password').type('salainen')
       cy.get('#login-button').click()
@@ -22,7 +22,7 @@ describe('Blog app', function() {
       cy.contains('Test User logged in')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
       cy.get('#username').type('testuser')
       cy.get('#password').type('wrong')
       cy.get('#login-button').click()
@@ -34,4 +34,36 @@ describe('Blog app', function() {
     })
   })
 
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'testuser', password: 'salainen' })
+    })
+
+    it('A blog can be created', function () {
+      cy.contains('new blog').click()
+      cy.get('#title').type('a test blog')
+      cy.get('#author').type('a test author')
+      cy.get('#url').type('a test url')
+      cy.get('#create-button').click()
+
+      cy.contains('a test blog')
+    })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'a test blog',
+          author: 'a test author',
+          url: 'a test url'
+        })
+      })
+
+      it.only('a blog can be liked', function () {
+        cy.contains('view').click()
+        cy.contains('like').click()
+        cy.contains('1 likes')
+      })
+    })
+
+  })
 })
--- part5/src/components/BlogForm.js
+++ part5/src/components/BlogForm.js
@@ -13,15 +13,15 @@ const BlogForm = ({ handleBlogCreate }) => {
       <h2>create new</h2>
       <form onSubmit={addBlog}>
         <div>
-          title: <input placeholder='title' type="text" value={blogInput.title} name="title" onChange={({ target }) => setBlogInput({ ...blogInput, title: target.value })} />
+          title: <input id="title" placeholder='title' type="text" value={blogInput.title} name="title" onChange={({ target }) => setBlogInput({ ...blogInput, title: target.value })} />
         </div>
         <div>
-          author: <input placeholder='author' type="text" value={blogInput.author} name="author" onChange={({ target }) => setBlogInput({ ...blogInput, author: target.value })} />
+          author: <input id="author" placeholder='author' type="text" value={blogInput.author} name="author" onChange={({ target }) => setBlogInput({ ...blogInput, author: target.value })} />
         </div>
         <div>
-          url: <input placeholder='url' type="text" value={blogInput.url} name="url" onChange={({ target }) => setBlogInput({ ...blogInput, url: target.value })} />
+          url: <input id="url" placeholder='url' type="text" value={blogInput.url} name="url" onChange={({ target }) => setBlogInput({ ...blogInput, url: target.value })} />
         </div>
-        <button type="submit">create</button>
+        <button id="create-button" type="submit">create</button>
       </form>
     </>
   )
