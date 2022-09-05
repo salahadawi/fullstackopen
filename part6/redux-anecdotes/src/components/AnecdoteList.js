c7ecfc56b52f37511d382c66b88f25de6193b775
Salah Adawi <salah.adawi@outlook.com>
2022-09-05 18:37:47 +0300

    part 6.12

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -3,8 +3,13 @@ import { voteAnecdote } from "../reducers/anecdoteReducer";
 import { setNotification, clearNotification } from "../reducers/notificationReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state.anecdotes)
   const dispatch = useDispatch()
+  const anecdotes = useSelector(state => {
+    if (state.filter === '')
+      return state.anecdotes
+    return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase()
+    .includes(state.filter.toLowerCase()))
+  })
 
   const vote = (id) => {
     dispatch(voteAnecdote(id))
