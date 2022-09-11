1b99ae143c1fef0406e1eabf560b412b2c10edb4
Salah Adawi <salah.adawi@outlook.com>
2022-09-11 19:16:14 +0300

    part 7.15

--- part7/blog-frontend/src/App.js
+++ part7/blog-frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 
 import { initializeBlogs } from './reducers/blogReducer'
 import { initializeUser } from './reducers/userReducer'
@@ -8,6 +8,7 @@ import { initializeUsers } from './reducers/usersReducer'
 
 import Display from './components/Display'
 import Users from './components/Users'
+import User from './components/User'
 import LogOutButton from './components/LogOut'
 import Notification from './components/Notification'
 
@@ -27,8 +28,13 @@ const App = () => {
     dispatch(initializeUsers())
   }, [dispatch])
 
-  if (!user) return <Display.LoggedOutDisplay />
+  const match = useMatch('/users/:id')
+  const users = useSelector((state) => state.users)
+  const userToDisplay = match
+    ? users.find((user) => user.id === match.params.id)
+    : null
 
+  if (!user) return <Display.LoggedOutDisplay />
   return (
     <div>
       <h2>blogs</h2>
@@ -40,6 +46,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Display.LoggedInDisplay />} />
         <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<User user={userToDisplay} />} />
       </Routes>
     </div>
   )
--- part7/blog-frontend/src/components/Users.test.js
+++ part7/blog-frontend/src/components/Users.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { screen } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Users from './Users'
 import { renderWithProviders } from '../tests/test_utils'
@@ -25,7 +26,12 @@ describe('<Users />', () => {
       ],
     }
 
-    renderWithProviders(<Users />, { preloadedState: initialState })
+    renderWithProviders(
+      <Router>
+        <Users />
+      </Router>,
+      { preloadedState: initialState }
+    )
 
     expect(screen.getByText('Users')).toBeInTheDocument()
     expect(screen.getByText('Test User')).toBeInTheDocument()
@@ -50,11 +56,42 @@ describe('<Users />', () => {
       ],
     }
 
-    renderWithProviders(<Users />, { preloadedState: initialState })
+    renderWithProviders(
+      <Router>
+        <Users />
+      </Router>,
+      { preloadedState: initialState }
+    )
 
     expect(screen.getByText('Users')).toBeInTheDocument()
     expect(screen.getByText('Test User')).toBeInTheDocument()
     expect(screen.getByText('Test User 2')).toBeInTheDocument()
     expect(screen.getAllByText('0')).toHaveLength(2)
+  })
+
+  test('users are clickable and link to user page', () => {
+    const initialState = {
+      users: [
+        {
+          username: 'testuser',
+          name: 'Test User',
+          id: '1',
+          blogs: [],
+        },
+      ],
+    }
+
+    renderWithProviders(
+      <Router>
+        <Users />
+      </Router>,
+      { preloadedState: initialState }
+    )
+
+    expect(screen.getByText('Test User')).toBeInTheDocument()
+    expect(screen.getByText('Test User').closest('a')).toHaveAttribute(
+      'href',
+      '/users/1'
+    )
   })
 })
