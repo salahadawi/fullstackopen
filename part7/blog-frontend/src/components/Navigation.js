87af11849aea34769f0e62505c919fd5a8469488
Salah Adawi <salah.adawi@outlook.com>
2022-09-17 12:26:53 +0300

    add divider after navbar

--- part7/blog-frontend/src/components/Navigation.js
+++ part7/blog-frontend/src/components/Navigation.js
@@ -3,29 +3,25 @@ import { useSelector } from 'react-redux'
 import LogOutButton from './LogOut'
 import NavigationButton from './NavigationButton'
 
-import { HStack, Flex } from '@chakra-ui/react'
+import { HStack, Flex, Box, Divider } from '@chakra-ui/react'
 
 const Navigation = () => {
   const user = useSelector((state) => state.user)
 
   return (
-    <Flex
-      w="100%"
-      px="6"
-      py="5"
-      align="center"
-      justify="space-between"
-      color="teal"
-    >
-      <HStack as="nav" spacing="5" m="auto">
-        <NavigationButton to="/">Blogs</NavigationButton>
-        <NavigationButton to="/users">Users</NavigationButton>
-      </HStack>
-      <HStack spacing="5">
-        <p>{user.name} logged in</p>
-        <LogOutButton />
-      </HStack>
-    </Flex>
+    <Box color="teal">
+      <Flex w="100%" px="6" py="4" align="center" justify="space-between">
+        <HStack as="nav" spacing="5" m="auto">
+          <NavigationButton to="/">Blogs</NavigationButton>
+          <NavigationButton to="/users">Users</NavigationButton>
+        </HStack>
+        <HStack spacing="5">
+          <p>{user.name} logged in</p>
+          <LogOutButton />
+        </HStack>
+      </Flex>
+      <Divider />
+    </Box>
   )
 }
 
