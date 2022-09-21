452ec475a1c286dc1366366bc888726d27a8af5f
Salah Adawi <salah.adawi@outlook.com>
2022-09-21 16:27:54 +0300

    fix navbar spacing

--- part7/blog-frontend/src/components/Navigation.js
+++ part7/blog-frontend/src/components/Navigation.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import LogOutButton from './LogOut'
 import NavigationButton from './NavigationButton'
 
-import { HStack, Flex, Box, Divider, Heading } from '@chakra-ui/react'
+import { HStack, Grid, Box, Divider, Heading } from '@chakra-ui/react'
 import { ChatIcon } from '@chakra-ui/icons'
 
 const Navigation = () => {
@@ -11,19 +11,25 @@ const Navigation = () => {
 
   return (
     <Box color="teal">
-      <Flex w="100%" px="6" py="4" justify="space-between">
+      <Grid
+        templateColumns="repeat(3, 1fr)"
+        w="100%"
+        px="6"
+        py="4"
+        justify="space-between"
+      >
         <HStack as="nav" spacing="5">
           <NavigationButton to="/">Blogs</NavigationButton>
           <NavigationButton to="/users">Users</NavigationButton>
         </HStack>
-        <Heading color="teal.900">
+        <Heading color="teal.900" textAlign="center">
           Blog app <ChatIcon />
         </Heading>
-        <HStack spacing="5">
+        <HStack spacing="5" justify="end">
           <p>{user.name} logged in</p>
           <LogOutButton />
         </HStack>
-      </Flex>
+      </Grid>
       <Divider />
     </Box>
   )
