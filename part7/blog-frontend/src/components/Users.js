e93c4271e96c3582119e86be3ec17a38e84a5131
Salah Adawi <salah.adawi@outlook.com>
2022-09-22 18:17:06 +0300

    add style for users display

--- part7/blog-frontend/src/components/Users.js
+++ part7/blog-frontend/src/components/Users.js
@@ -1,30 +1,47 @@
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import {
+  Center,
+  Heading,
+  VStack,
+  TableContainer,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from '@chakra-ui/react'
+
 const Users = () => {
   const users = useSelector((state) => state.users)
   return (
-    <div>
-      <h2>Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>blogs created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>
-                <Link to={`/users/${user.id}`}>{user.name}</Link>
-              </td>
-              <td>{user.blogs.length}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Center>
+      <VStack spacing="4" bg="teal.50" px="64px" py="48px" rounded="xl" m="8">
+        <Heading>Users</Heading>
+        <TableContainer>
+          <Table variant="striped" colorScheme="teal">
+            <Thead>
+              <Tr>
+                <Th>Username</Th>
+                <Th isNumeric>blogs created</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {users.map((user) => (
+                <Tr key={user.id}>
+                  <Td>
+                    <Link to={`/users/${user.id}`}>{user.name}</Link>
+                  </Td>
+                  <Td isNumeric>{user.blogs.length}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </VStack>
+    </Center>
   )
 }
 
