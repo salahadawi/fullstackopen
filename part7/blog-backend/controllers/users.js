1ca30448996891c9db7a35018431fb6256ed7525
Salah Adawi <salah.adawi@outlook.com>
2022-09-22 19:13:55 +0300

    populate comments for blogs when fetching user

--- part7/blog-backend/controllers/users.js
+++ part7/blog-backend/controllers/users.js
@@ -3,7 +3,14 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1, id: 1 })
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+    id: 1,
+    comments: 1,
+  })
   response.json(users)
 })
 
@@ -11,7 +18,9 @@ usersRouter.post('/', async (request, response) => {
   const body = request.body
 
   if (!body.password || body.password.length < 3) {
-    return response.status(400).json({ error: 'password must be at least 3 characters long' })
+    return response
+      .status(400)
+      .json({ error: 'password must be at least 3 characters long' })
   }
 
   if (await User.findOne({ username: body.username }))
