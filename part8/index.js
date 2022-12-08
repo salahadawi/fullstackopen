03963231a8f570415fbb659d5186cc7a33b877c5
Salah Adawi <salah.adawi@outlook.com>
2022-12-08 14:56:27 +0200

    part 8.2

--- part8/index.js
+++ part8/index.js
@@ -93,9 +93,16 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -103,6 +110,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
