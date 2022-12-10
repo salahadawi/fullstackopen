4afa1b55f7704580576783bc4346df5f1879127b
Salah Adawi <salah.adawi@outlook.com>
2022-12-10 22:07:41 +0200

    part 8.18

--- part8/frontend/src/App.js
+++ part8/frontend/src/App.js
@@ -1,24 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useApolloClient } from "@apollo/client";
+
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
+};
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const client = useApolloClient();
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 5000);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+    setPage("authors");
+  };
+
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem("books-user-token");
+    if (loggedUser) {
+      setToken(loggedUser);
+    }
+  }, []);
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {!token ? (
+          <button onClick={() => setPage("loginForm")}>login</button>
+        ) : (
+          <>
+            <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={() => logout()}>logout</button>
+          </>
+        )}
       </div>
-
+      <Notify errorMessage={errorMessage} />
       <Authors show={page === "authors"} />
-
       <Books show={page === "books"} />
-
       <NewBook show={page === "add"} />
+      <LoginForm
+        show={page === "loginForm"}
+        setToken={setToken}
+        setError={notify}
+      />
     </div>
   );
 };
--- part8/frontend/src/queries.js
+++ part8/frontend/src/queries.js
@@ -14,7 +14,9 @@ export const GET_ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
       genres
     }
@@ -35,7 +37,9 @@ export const CREATE_BOOK = gql`
       genres: $genres
     ) {
       title
-      author
+      author {
+        name
+      }
       published
       genres
     }
@@ -47,6 +51,14 @@ export const EDIT_AUTHOR = gql`
     editAuthor(name: $name, setBornTo: $setBornTo) {
       name
       born
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
     }
   }
 `;
