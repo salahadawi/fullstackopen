984666bb27092b8bf3d8bd5b7abeaabf32ed1670
Salah Adawi <salah.adawi@outlook.com>
2022-12-15 16:10:06 +0200

    part 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
