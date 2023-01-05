270dec1f0c28b50635435680b5048383b4378e22
Salah Adawi <salah.adawi@outlook.com>
2023-01-05 16:50:25 +0200

    part 10.11

---
+++ part10/src/hooks/useRepositories.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@apollo/client";
+
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  const { data, loading, error } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  return { repositories: data?.repositories, loading, error };
+};
+
+export default useRepositories;
