50c7531267143e422e816895e9535609bb8d51a1
Salah Adawi <salah.adawi@outlook.com>
2023-01-05 18:32:42 +0200

    part 10.14

---
+++ part10/src/utils/authStorage.js
@@ -0,0 +1,21 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+class AuthStorage {
+  constructor(namespace = "auth") {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    return await AsyncStorage.getItem(`${this.namespace}:token`);
+  }
+
+  async setAccessToken(accessToken) {
+    await AsyncStorage.setItem(`${this.namespace}:token`, accessToken);
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:token`);
+  }
+}
+
+export default AuthStorage;
